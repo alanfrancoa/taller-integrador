f9a66ffc4db1c29e756f1967016fcb2aadbc88fe
Alan Alvarez <alvarezalanfranco@gmail.com>
2025-03-23 19:27:18 -0300

    Se realiza el ejemplo del inciso 5

---
+++ Ejercicios/01-funciones/05-funciones.py
@@ -0,0 +1,13 @@
+'''
+Fun5. Dados tres números a, b, c, regresar su suma. En otras palabras, hay que escribir una función con tres argumentos que calcule y regrese la suma de sus argumentos.
+Ejemplo. Entrada: 25, −3, 17. Salida: 39. 
+'''
+
+def sumaTresNumeros (a, b, c):
+    return a+b+c
+
+numA = 25
+numB = -3
+numC = 17
+
+print("Resultado: " + str(sumaTresNumeros(numA, numB, numC)))
